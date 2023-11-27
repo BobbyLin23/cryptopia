90a1f6c1126cfaf75c8b2a14c11b6e81ff1abf27
FrankLin23 <linzhangsheng23@163.com>
2023-11-27 23:22:27 +0800

    feat: add landing page

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -3,14 +3,17 @@
 </script>
 
 <template>
-  <nav
-    class="w-full px-6 py-4 flex items-center
-    border-b dark:border-gray-700"
+  <header
+    class="backdrop-blur border-b sticky -mb-px top-0 z-50
+    border-gray-200/75 dark:border-gray-700/50"
   >
-    <h1 class="text-2xl font-semibold">
-      Cryptopia
-    </h1>
-    <div class="flex-auto" />
-    <ModeToggle />
-  </nav>
+    <div class="mx-auto px-4 sm:px-5 lg:px-8 max-w-7xl flex items-center justify-between gap-3 h-16">
+      <h1 class="text-2xl font-semibold">
+        Cryptopia
+      </h1>
+      <div class="flex-auto" />
+      <UButton icon="i-heroicons-document-duplicate-solid" variant="ghost" color="gray" />
+      <ModeToggle />
+    </div>
+  </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-const { x, y } = useMouse()
+
 </script>
 
 <template>
-  <div class="p-4 text-rose-300">
-    hello world
-    {{ x }}, {{ y }}
+  <div class="min-h-full py-24">
+    <div class="flex flex-col items-center gap-y-10">
+      <h1 class="text-7xl text-sky-500">
+        Cryptopia
+      </h1>
+      <UButton size="lg" label="Get Started" color="sky" @click="$router.push('/dashboard')" />
+    </div>
   </div>
 </template>
